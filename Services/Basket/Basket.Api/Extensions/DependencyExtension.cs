6924973215e17631d5c96580d4e09ef3fb662fef
sowad <ashfaqulislam4566@gmail.com>
2024-09-21 23:11:12 +0600

    BasketserviceIntial

---
+++ Services/Basket/Basket.Api/Extensions/DependencyExtension.cs
@@ -0,0 +1,66 @@
+﻿
+using EShopping.Core.Middleware;
+
+namespace Basket.Api.Extensions
+{
+    public static class DependencyConfiguration
+    {
+        public static IServiceCollection AddDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            ILogger<Startup> logger = services
+                           .BuildServiceProvider()
+                           .GetRequiredService<ILogger<Startup>>();
+
+            services.AddSwaggerService(configuration);
+            services.AddMemoryCache();
+
+            //services.AddAutoMapper(typeof(Startup));
+
+            //services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining(typeof(Startup)));
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
+            // Options
+            services.AddOptions();
+         
+
+            // Add Database Context
+            services.AddDatabaseContextService(configuration, logger);
+            services.AddScoped<ISession>(provider =>
+            provider
+                .GetRequiredService<IHttpContextAccessor>()
+                .HttpContext
+                .Session);
+           
+
+          
+
+
+        
+            return services;
+        }
+
+        public static IApplicationBuilder UseDependencies(
+            this IApplicationBuilder app,
+            IConfiguration configuration,
+            //ILoggerFactory loggerFactory,
+
+            IServiceProvider serviceProvider
+        )
+        {
+
+            app.UseMiddleware<GlobalExceptionHandlingMiddleware>();
+
+            app.UseSwaggerService();
+         
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+            return app;
+        }
+    }
+}
---
+++ Services/Basket/Basket.Api/Startup.cs
@@ -0,0 +1,39 @@
+﻿
+
+using Basket.Api.Extensions;
+
+namespace Basket.Api
+{
+   
+        public class Startup
+        {
+            public IConfiguration Configuration;
+            public Startup(IConfiguration configuration)
+            {
+                Configuration = configuration;
+            }
+            public void ConfigureServices(IServiceCollection services)
+            {
+                services.AddControllers();
+
+                services.AddDependencies(Configuration);
+            }
+            public void Configure(IApplicationBuilder app,
+                IWebHostEnvironment env,
+                IConfiguration configuration,
+                IServiceProvider serviceProvider)
+            {
+                if (env.IsDevelopment())
+                {
+                    app.UseDeveloperExceptionPage();
+
+                }
+                app.UseDependencies(configuration, serviceProvider);
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                });
+            }
+        }
+    }
+
